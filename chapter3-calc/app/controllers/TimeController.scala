94e4ccf9545ff9c61cc61287b4c88d34384cfa97
Dave Gurnell <dave@untyped.com>
2015-01-23 12:02:56 +0000

    Added `chapter3-calc` solution

---
+++ chapter3-calc/app/controllers/TimeController.scala
@@ -0,0 +1,29 @@
+package controllers
+
+import java.util.Date
+import org.joda.time.{DateTime, DateTimeZone}
+import org.joda.time.format.DateTimeFormat
+import play.api._
+import play.api.mvc._
+
+object CalcController extends Controller {
+  def add(a: Int, b: Int) = Action { request =>
+    Ok((a + b).toString)
+  }
+
+  def and(a: Boolean, b: Boolean) = Action { request =>
+    Ok((a && b).toString)
+  }
+
+  def concat(args: String) = Action { request =>
+    Ok(args.split("/").map(decode).mkString)
+  }
+
+  def howToAdd(a: Int, b: Int) = Action { request =>
+    val call = routes.CalcController.add(a, b)
+    Ok(call.method + " " + call.url)
+  }
+
+  private def decode(str: String) =
+    java.net.URLDecoder.decode(str, "UTF-8")
+}
