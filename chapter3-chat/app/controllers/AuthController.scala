b379da48d55db9d90d9afb125986cd309ae4368a
Dave Gurnell <dave@untyped.com>
2015-01-27 16:05:13 +0000

    Tweaks to comments in chapter3-chat

--- chapter3-chat/app/controllers/AuthController.scala
+++ chapter3-chat/app/controllers/AuthController.scala
@@ -12,8 +12,8 @@ object AuthController extends Controller {
   //     - If it's LoginSuccess, return an Ok response that sets a cookie
   //     - If it's UserNotFound or PasswordIncorrect, return a BadRequest response
   //
-  // NOTE: We don't know how to create HTML yet, so populate each response
-  // with a plain text message.
+  // NOTE: We don't know how to create HTML yet,
+  // so populate each response with a plain text message.
   def login(username: Username, password: Password) = Action { request =>
     ???
   }
--- chapter3-chat/app/controllers/ChatController.scala
+++ chapter3-chat/app/controllers/ChatController.scala
@@ -15,8 +15,8 @@ object ChatController extends Controller {
   //     - If they are, return an Ok response containing a list of messages
   //     - If they aren't, redirect to the login page
   //
-  // NOTE: We don't know how to create HTML yet, so populate each response
-  // with a plain text message.
+  // NOTE: We don't know how to create HTML yet,
+  // so populate each response with a plain text message.
   def index = Action { request =>
     ???
   }
@@ -26,8 +26,8 @@ object ChatController extends Controller {
   //     - If they are, create a message from the relevant author
   //     - If they aren't, redirect to the login page
   //
-  // NOTE: We don't know how to create HTML yet, so populate each response
-  // with a plain text message.
+  // NOTE: We don't know how to create HTML yet,
+  // so populate each response with a plain text message.
   def submitMessage(text: String) = Action { request =>
     ???
   }
